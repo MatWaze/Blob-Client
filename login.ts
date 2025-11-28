68fa56e0418bd192b804ddcc860afc8c161da33b
MatWaze <coolmartun@gmail.com>
2025-11-28 21:57:03 +0400

    network error fixed

--- login.ts
+++ login.ts
@@ -76,7 +76,7 @@ form.addEventListener('submit', async (e: Event) => {
 	successDiv.textContent = '';
 
 	try {
-		const res = await fetch('/api/users/login', {
+		const res = await fetch('http://localhost:4000/api/users/login', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
