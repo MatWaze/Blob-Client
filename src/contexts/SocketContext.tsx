9999af4b185c5b6f8e93bbf35b81c7c8ad5a1065
MatWaze <coolmartun@gmail.com>
2026-03-08 22:30:58 +0300

    fix

--- src/contexts/SocketContext.tsx
+++ src/contexts/SocketContext.tsx
@@ -25,7 +25,7 @@ export const SocketProvider: React.FC<{ children: ReactNode }> = ({ children }) 
         if (!isAuthenticated) return;
 
         let ws: WebSocket | null = null;
-        let reconnectTimeout: NodeJS.Timeout;
+        let reconnectTimeout;
         let isActive = true;
 
         const connect = async () => {
